877034e3477a07f533624af52f74279927424288
Agustin Pontura <pontura@gmail.com>
2021-11-03 21:11:00 -0300

    anims

--- genero/Assets/scripts/CharacterManager.cs
+++ genero/Assets/scripts/CharacterManager.cs
@@ -4,41 +4,82 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
+    [SerializeField] float timeForVersion1;
+    [SerializeField] float timeForVersion2;
+
     public Animation character_anim;
     public Animation mouth_anim;
     public float minSensibilityValue = .001f;
     bool isTalking;
     AudioSpectrum audioSpectrum;
 
+    public int versionID;
+
+    public GameObject splash;
+    public GameObject uiTexts;
+
     void Start()
     {
         audioSpectrum = Data.Instance.audioSpectrum;
+        Reset();
         Shutup();
         LoopTalk();
     }
+    private void Init()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TalkingC());
+        Events.PlaySound("voices", "voices/version" + versionID, false);
+        splash.SetActive(false);
+        uiTexts.SetActive(true);
+        uiTexts.GetComponent<Animation>().Play("version" + versionID);
+    }
+    IEnumerator TalkingC()
+    {
+        float delay = timeForVersion1;
+        if (versionID == 2)
+            delay = timeForVersion2;
+
+        print("anima hasta: " + delay);
+
+        yield return new WaitForSeconds(delay);
+
+        print("Reset");
+        Reset();
+    }
+    public void Reset()
+    {
+        StopAllCoroutines();
+        splash.SetActive(true);
+        uiTexts.SetActive(false);
+        Events.PlaySound("voices", "", false);
+    }
     public void Shutup()
     {
-        print("_________Shutup");
         mouth_anim.Play("shutup");
         isTalking = false;
     }
     public void Talk()
     {
-        print("Talk");
         if (!isTalking)
             mouth_anim.Play("talk");
         isTalking = true;
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetMouseButtonDown(0))
         {
             if (!isTalking)
-                Events.PlaySound("voices", "voices/test", false);
+            {
+                Init();
+            }
             else
-                Events.PlaySound("voices", "", false); 
+            {
+                Reset();
+            }
         }
     }
+
     void LoopTalk()
     {
         if (audioSpectrum.result > minSensibilityValue)
